129317edf8fd77d121bc77e41c8756711e5526b6
mahdikarimi422@gmail.com <09388990352@>
2022-05-09 11:51:43 +0430

    create project.

---
+++ src/main/java/ir/maktab/maktabprojectstep2/MaktabProjectStep2Application.java
@@ -0,0 +1,13 @@
+package ir.maktab.maktabprojectstep2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaktabProjectStep2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaktabProjectStep2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ir/maktab/maktabprojectstep2/MaktabProjectStep2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maktab.maktabprojectstep2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaktabProjectStep2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
