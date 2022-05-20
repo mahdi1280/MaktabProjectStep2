1a9e92aa87dcc43498ccb7bf5011f8f727809109
mahdikarimi422@gmail.com <09388990352@>
2022-05-20 13:50:29 +0430

    insert http file axios.

--- front/achare/src/App.js
+++ front/achare/src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">آچاره</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                  aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#">خدمت</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">خدمت</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                   data-bs-toggle="dropdown" aria-expanded="false">
+                  خدمت ها
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                  <li><a className="dropdown-item" href="#">خدمت</a></li>
+                  <li><a className="dropdown-item" href="#">خدمت</a></li>
+                  <li><a className="dropdown-item" href="#">خدمت</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="جست و جو" aria-label="Search"/>
+              <button className="btn btn-outline-success" type="submit">search</button>
+            <button className="btn btn-sm btn-info">ورود ثبت نام</button>
+          </form>
+        </div>
+      </nav>
     </div>
   );
 }
