d61de9101cc5836b75a858834341fe826f0ae8bd
mahdikarimi422@gmail.com <09388990352@>
2022-05-20 22:22:10 +0430

    login user page

--- front/achare/src/Pages/Login/Login.jsx
+++ front/achare/src/Pages/Login/Login.jsx
@@ -1,7 +1,37 @@
 import Header from "../../Components/Header";
-import {Link} from "react-router-dom";
+import {Link,useHistory} from "react-router-dom";
 import InputText from "../../Components/InputText";
+import {useState} from "react";
+import {post} from '../../http';
+import {setUser} from '../../Auth';
+
 export default function Login() {
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+    const [error, setError] = useState([]);
+    const history = useHistory();
+    const [loading,setLoading] = useState(false);
+    function handlerLogin(){
+        setLoading(true);
+        post("/customer/login",{
+            email:email,
+            password:password
+        })
+            .then((response)=>{
+                setLoading(false);
+                setUser(response.data.token,response.data.id);
+                localStorage.setItem("token",response.data.token);
+                localStorage.setItem("id",response.data.id);
+
+                history.replace("/");
+            }).catch((response)=>{
+                setError(response.response.data);
+        })
+    }
+
+    const errors = error.map(response =>
+        <li>{response.defaultMessage ? response.defaultMessage : response.message}</li>);
+
     return (<><Header/>
             <div className="col-sm-5 form">
                 <ul className="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
@@ -21,23 +51,20 @@ export default function Login() {
                 <div className="tab-content">
                     <div className="tab-pane fade show active" id="pills-login" role="tabpanel"
                          aria-labelledby="tab-login">
-                        <form>
-
-
-                            <InputText id={"registerEmail"} type={"email"} value={"ایمیل:"}/>
-                            <InputText id={"registerPassword"} type={"password"} value={"گذر واژه:"}/>
-
-
+                        {errors.length !== 0 &&
+                        <div className="alert alert-danger" role="alert">
+                            {errors}
+                        </div>
+                        }
+                            <InputText changeHandler={(e)=>setEmail(e.target.value)} id={"registerEmail"} type={"email"} value={"ایمیل:"}/>
+                            <InputText changeHandler={(e)=>setPassword(e.target.value)} id={"registerPassword"} type={"password"} value={"گذر واژه:"}/>
                             <div className="row mb-4">
 
                                 <div className="col-md-6 d-flex justify-content-center">
                                     <a>گذرواژه خود را فراموش کرده اید!</a>
                                 </div>
                             </div>
-
-                            <button type="submit" className="btn btn-primary btn-block mb-4">ورود</button>
-
-                        </form>
+                            <button onClick={handlerLogin} className="btn btn-primary btn-block mb-4">ورود</button>
                     </div>
                 </div>
             </div>
--- src/main/java/ir/maktab/maktabprojectstep2/dto/response/LoginSaveResponse.java
+++ src/main/java/ir/maktab/maktabprojectstep2/dto/response/LoginSaveResponse.java
@@ -2,9 +2,11 @@ package ir.maktab.maktabprojectstep2.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 @AllArgsConstructor
 @Builder
+@Getter
 public class LoginSaveResponse {
 
     private final long id;
